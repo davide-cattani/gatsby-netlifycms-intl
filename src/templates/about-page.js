29e3565cbe930b7defe31cdff5956ef0242f758c
Davide Cattani <davide.cattani@studioilgranello.it>
2021-06-15 08:50:33 +0200

    Merge branch 'main' of https://github.com/davide-cattani/sketch-studios into main

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -8,16 +8,21 @@ export const pageQuery = graphql`
   query AboutQuery($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
-      html
-      excerpt(pruneLength: 140)
       frontmatter {
         title
+        coworkingDescription
+        cta {
+          ctaLink
+          ctaText
+        }
       }
+      html
+      excerpt(pruneLength: 150)
     }
   }
 `
 const AboutPage = ({ data }) => {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
+  const { markdownRemark } = data
   const { frontmatter, html, excerpt } = markdownRemark
 
   return (
@@ -27,6 +32,15 @@ const AboutPage = ({ data }) => {
         <div className="section">
           <h1 className="title is-size-2">{frontmatter.title}</h1>
           <article dangerouslySetInnerHTML={{ __html: html }} />
+          <div className="buttons is-centered">
+            <Link to={frontmatter.cta.ctaLink} className="button is-primary is-large my-6">
+              {frontmatter.cta.ctaText}
+            </Link>
+          </div>
+        </div>
+        <div className="section">
+          <h1 className="title is-size-2">Lo spazio di co-working</h1>
+          <article dangerouslySetInnerHTML={{ __html: frontmatter.coworkingDescription }} />
         </div>
       </div>
     </Layout>
