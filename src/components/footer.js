a9604f66b5c038cd966eb9ab3b8d6d4cd54dddda
Davide Cattani <davide.cattani@studioilgranello.it>
2021-06-24 17:00:39 +0200

    correzione stile footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,29 @@
 /** @jsx jsx */
+import {React} from "react"
 import { jsx } from "theme-ui"
 import { RiHeart2Line } from "react-icons/ri"
 
-const Footer = () => (
-  <footer
-    className="footer"
-    sx={{ bg: "primaryColor", }}
-  >
-    <div className="content has-text-centered">
-      <p>
-        Fatto con il{" "}
-        <span style={{color: 'red'}}>
-          <RiHeart2Line />
-        </span>{" "}
-        da <a href="https://www.davidecattani.dev">catta</a>
-      </p>
-    </div>
-  </footer>
-)
+const Footer = () => {
+
+  return (
+    <footer
+      className="footer"
+      sx={{ bg: "primaryColor", }}
+    >
+      <div className="content has-text-centered">
+        <p className="mb-6">
+          Fatto con il{" "}
+          <span style={{color: 'red'}}>
+            <RiHeart2Line />
+          </span>{" "}
+          da <a href="https://www.davidecattani.dev">catta</a>
+        </p>
+        <button className="button is-small is-dark is-outlined" onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>
+          Torna su
+        </button>
+      </div>
+    </footer>
+  )
+} 
 
 export default Footer
